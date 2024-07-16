7318c79ed0a61745598665b0762b986b7029b0fe
Cosmic Outlaw <hello@phillomenal.com>
2024-07-16 18:44:17 +0200

    better doc

---
+++ R/phillomenal-package.R
@@ -0,0 +1,8 @@
+#' phillomenal: A package for analyzing U.S. House voting data
+#'
+#' This package includes data sets of all United States Congress members ("HSall_members") and their votes of selected Congresses ("HS_votes"),
+#' namely 91, 97, 102, 107 and 117.
+#'
+#' @docType package
+#' @name phillomenal
+NULL
